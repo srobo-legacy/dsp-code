c7887747ff647403c7c6f70ece2aa24fb22dddee
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-08 18:31:04 +0100

    Parse a particular test and forward to run_test

--- tests/arm/dsptest.c
+++ tests/arm/dsptest.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,55 @@ useage()
 
 	fprintf(stderr, "Usage: dsptest testcases.xml\n");
 	return;
+}
+
+int
+parse_test(xmlDocPtr doc, xmlNodePtr test)
+{
+	xmlNodePtr subnode;
+	const char *testname, *tmp, *endptr;
+	int testnum;
+	int wanted_retval;
+	bool got_retval;
+
+	testname = NULL;
+	testnum = -1;
+	got_retval = false;
+
+	subnode = test->xmlChildrenNode;
+	while (subnode != NULL) {
+		if (!xmlStrcmp("name", subnode->name)) {
+			testname = xmlNodeListGetString(doc,
+					subnode->xmlChildrenNode, 1);
+		} else if (!xmlStrcmp("nodenum", subnode->name)) {
+			tmp = xmlNodeListGetString(doc,
+					subnode->xmlChildrenNode, 1);
+			testnum = strtol(tmp, &endptr, 16);
+			if (entptr == tmp) {
+				fprintf(stderr, "\"%s\" is not a node number\n",
+									tmp);
+				return -1;
+			}
+		} else if (!xmlStrcmp("retval", subnode->name)) {
+			tmp = xmlNodeListGetString(doc,
+					subnode->xmlChildrenNode, 1);
+			wanted_retval = strtol(tmp, &endptr, 16);
+			got_retval = true;
+			if (entptr == tmp) {
+				fprintf(stderr, "\"%s\" is not a return val\n",
+									tmp);
+				return -1;
+			}
+		}
+		subnode = subnode->next;
+	}
+
+	if (got_retval == false || testnum == -1 || testname == NULL) {
+		fprintf(stderr, "Missing node(s) when reading a test\n");
+		return -1;
+	}
+
+	return run_test(testname, testnum, wanted_retval);
 }
 
 int
