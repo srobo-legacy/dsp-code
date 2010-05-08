2e66a656d26993cc2f79c71749769d8a84f5c8c7
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-08 23:46:12 +0100

    Add start.c, with test function which simply returns zero

---
+++ tests/dsp/start.c
@@ -0,0 +1,14 @@
+#define SR_NODE_GUID_STRING "FACEBEE5_FACE_BEE5_FACE_BEE500000000"
+#define SR_NODE_CREATE_FUNC "main"
+#define SR_NODE_EXECUTE_FUNC "main"
+#define SR_NODE_DELETE_FUNC "main"
+#define SR_NODE_NAME "test return 0"
+
+#include "sr_dcd.h"
+
+int
+main()
+{
+
+	return 0;
+}
