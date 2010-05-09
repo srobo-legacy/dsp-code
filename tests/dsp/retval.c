02329b2d98786d8fe041ff850bb16b0f2535eef5
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-09 14:24:02 +0100

    Add specific-return-code test and void function call test

---
+++ tests/dsp/retval.c
@@ -0,0 +1,18 @@
+#define SR_TEST_NAME "bees"
+#define SR_TEST_RET_VAL -1
+#define SR_TEST_FILENAME retval.doff
+#define SR_TEST_NUM 1
+#define SR_TEST_NUM_STR "00000001"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+int
+main()
+{
+
+	return -1;
+}
+
+#endif
---
+++ tests/dsp/voidcall.c
@@ -0,0 +1,26 @@
+#define SR_TEST_NAME "bees"
+#define SR_TEST_RET_VAL 0
+#define SR_TEST_FILENAME voidcall.doff
+#define SR_TEST_NUM 2 
+#define SR_TEST_NUM_STR "00000002"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+void
+bees()
+{
+
+	return;
+}
+
+int
+main()
+{
+
+	bees();
+	return 0;
+}
+
+#endif
