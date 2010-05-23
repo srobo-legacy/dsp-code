162960bb0678e3c681db1033ceb6794d89747f92
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-23 19:02:56 +0100

    Add argcall test - verifies argument received and returned by function

---
+++ tests/dsp/argcall.c
@@ -0,0 +1,25 @@
+#define SR_TEST_NAME "Check argument reaches function"
+#define SR_TEST_RET_VAL 0x2A
+#define SR_TEST_FILENAME argcall.doff
+#define SR_TEST_NUM 3
+#define SR_TEST_NUM_STR "00000003"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+int
+bees(int i)
+{
+
+	return i;
+}
+
+int
+test_main()
+{
+
+	return bees(0x2A);
+}
+
+#endif
