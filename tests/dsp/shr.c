df3fd9ed6b201e3bf8e224d4525f56f263d2da46
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-24 00:39:13 +0100

    Adjust shr test - should sign extend result

--- tests/dsp/shr.c
+++ tests/dsp/shr.c
@@ -1,7 +1,7 @@
 #define SR_TEST_NAME "Test shr operation"
 #define SR_TEST_RET_VAL 7
 #define SR_TEST_FILENAME shr.doff
-#define SR_TEST_NUM 7
+#define SR_TEST_NUM 0xFFFFFF00
 #define SR_TEST_NUM_STR "00000007"
 
 #include "sr_test.h"
@@ -12,14 +12,14 @@ int
 test(int a)
 {
 
-	return a >> 12;
+	return a >> 8;
 }
 
 int
 test_main()
 {
 
-	return test(0x7554);
+	return test(0xFFFF0000);
 }
 
 #endif
