2cb4b7b8e51122e0e702e03789873c8d71323a74
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-24 00:45:21 +0100

    Correct flipped testno/retval, should be other way round

--- tests/dsp/shr.c
+++ tests/dsp/shr.c
@@ -1,7 +1,7 @@
 #define SR_TEST_NAME "Test shr operation"
-#define SR_TEST_RET_VAL 7
+#define SR_TEST_RET_VAL 0xFFFFFF00
 #define SR_TEST_FILENAME shr.doff
-#define SR_TEST_NUM 0xFFFFFF00
+#define SR_TEST_NUM 7
 #define SR_TEST_NUM_STR "00000007"
 
 #include "sr_test.h"
