f284545de2c865c4c7491fe081de0b647570d3c1
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-24 13:27:48 +0100

    Ensure that cmplt failing won't replicate the success exit code

--- tests/dsp/cmplt.c
+++ tests/dsp/cmplt.c
@@ -1,5 +1,5 @@
 #define SR_TEST_NAME "Test < operation"
-#define SR_TEST_RET_VAL 0x8
+#define SR_TEST_RET_VAL 0x4
 #define SR_TEST_FILENAME cmplt.doff
 #define SR_TEST_NUM D
 #define SR_TEST_NUM_STR "0000000D"
@@ -19,7 +19,7 @@ int
 test_main()
 {
 
-	return test(2) + test(3) + test(4) + test(5);
+	return test(3) + test(4) + test(4) + test(5) + test(5);
 }
 
 #endif
