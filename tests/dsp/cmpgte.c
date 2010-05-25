dfbb587377de94899acfcf24a7da35b0305315ee
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-25 03:00:29 +0100

    Add cmpgte and cmplte tests

---
+++ tests/dsp/cmpgte.c
@@ -0,0 +1,25 @@
+#define SR_TEST_NAME "Test greater-than-or-equal operation"
+#define SR_TEST_RET_VAL 0xC
+#define SR_TEST_FILENAME cmpgte.doff
+#define SR_TEST_NUM 10
+#define SR_TEST_NUM_STR "00000010"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+int
+test(int a)
+{
+
+	return (a >= 6) ? 3 : 0;
+}
+
+int
+test_main()
+{
+
+	return test(5) + test(5) + test(6) + test(6) + test(7) + test(7);
+}
+
+#endif
---
+++ tests/dsp/cmplte.c
@@ -0,0 +1,25 @@
+#define SR_TEST_NAME "Test less-than-or-equal operation"
+#define SR_TEST_RET_VAL 0x44
+#define SR_TEST_FILENAME cmplte.doff
+#define SR_TEST_NUM F
+#define SR_TEST_NUM_STR "0000000F"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+int
+test(int a)
+{
+
+	return (a <= 9) ? 17 : 0;
+}
+
+int
+test_main()
+{
+
+	return test(8) + test(8) + test(9) + test(9) + test(10) + test(10);
+}
+
+#endif
