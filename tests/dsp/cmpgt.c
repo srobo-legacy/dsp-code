90aaebdec05641d54bde5427ac25d4e77a96033b
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-25 02:39:05 +0100

    Add compare-greater-than test

---
+++ tests/dsp/cmpgt.c
@@ -0,0 +1,25 @@
+#define SR_TEST_NAME "Test greater-than operation"
+#define SR_TEST_RET_VAL 0x14
+#define SR_TEST_FILENAME cmplt.doff
+#define SR_TEST_NUM E
+#define SR_TEST_NUM_STR "0000000E"
+
+#include "sr_test.h"
+
+#ifndef GENERATE_XML
+
+int
+test(int a)
+{
+
+	return (a > 9) ? 10 : 0;
+}
+
+int
+test_main()
+{
+
+	return test(8) + test(8) + test(9) + test(9) + test(10) + test(10);
+}
+
+#endif
