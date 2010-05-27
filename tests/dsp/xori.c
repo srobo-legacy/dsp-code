14dc4e96e71fb0a420ffaaae09a1886f403f4880
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-27 03:35:17 +0100

    Correct xori expected return value, add explanatory comment

--- tests/dsp/xori.c
+++ tests/dsp/xori.c
@@ -1,5 +1,5 @@
 #define SR_TEST_NAME "Test xor-immediate operation"
-#define SR_TEST_RET_VAL 0x3
+#define SR_TEST_RET_VAL 0x5
 #define SR_TEST_FILENAME xori.doff
 #define SR_TEST_NUM_STR "00000016"
 
@@ -25,6 +25,8 @@ int
 test_main()
 {
 
+	/* 0 + 3 + 2 + 5 + 4 = 14
+	 * -2, -3, -4, totals 5 */
 	return test(1) + test(2) + test(3) + test(4) + test(5)
 		+ test2(1) + test2(2) + test2(3);
 }
