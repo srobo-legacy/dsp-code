4ab88caa61b5910e90cd284e5ca01ef45d6f2439
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-06-13 10:31:27 +0100

    Add code to dectect when dsp receives stream buffer

--- tests/stream/dsp-stream.c
+++ tests/stream/dsp-stream.c
@@ -81,8 +81,27 @@ create(int arg_len, char *arg_str, int num_in_streams,
 int
 execute(NODE_EnvPtr node)
 {
+	struct state *s;
+	uint8_t *in_buf;
+	uint32_t context;
+	Uns streams, msgs; /* Uns? Sounds like a war crime to me */
 
-	/* do some stuff */
+	s = node->moreEnv;
+	SYS_printf("In execute, state ptr %x\n", s);
+	/* "Prime" input stream with buffer (as said by strm example) */
+	STRM_issue(s->in_handle, s->in_buf, s->in_size, s->in_size, 0);
+
+	streams = NODE_wait(node, &s->in_handle, 1, 10000, &msgs);
+	SYS_printf("sys streams %d, msgs %d\n", streams, msgs);
+	if (streams == 0)
+		panic();
+
+	STRM_reclaim(s->in_handle, &in_buf, NULL, &context);
+
+	if (in_buf == NULL)
+		panic();
+
+	return 0;
 }
 
 int
