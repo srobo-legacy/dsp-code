922ce67cfef2cda1303a8518c670f7961725f645
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-08-09 17:35:16 +0100

    Build fixes for dsp side

--- tests/ping/dsp.c
+++ tests/ping/dsp.c
@@ -1,4 +1,4 @@
-#define SR_NODE_GUID_STRING "3CBF0A73-5D16-4B0C-9EF9-3CBAC583886E"
+#define SR_NODE_GUID_STRING "3CBF0A73_5D16_4B0C_9EF9_3CBAC583886E"
 #define SR_NODE_CREATE_FUNC "create"
 #define SR_NODE_EXECUTE_FUNC "execute"
 #define SR_NODE_DELETE_FUNC "delete"
@@ -10,6 +10,7 @@
 
 #include <dspapi.h>
 
+int
 create(int arg_len, char *arg_str, int num_in_streams,
 		uint32_t in_stream_handles[], int num_out_streams,
 		uint32_t out_stream_handles[], NODE_EnvPtr node)
@@ -23,7 +24,7 @@ int
 execute(NODE_EnvPtr node)
 {
 	RMS_DSPMSG msg, msg2;
-	int msgs;
+	unsigned int msgs;
 
 	while (1) {
 		NODE_wait(node, NULL, 0, 10000, &msgs);
