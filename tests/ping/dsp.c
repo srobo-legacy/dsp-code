e1755ae2e945f51c2353757f16fc8e73f0dbe652
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-08-09 17:18:27 +0100

    Remove stream-specific stuff up to create routine;
    We require no state or setup to pass messages

--- tests/ping/dsp.c
+++ tests/ping/dsp.c
@@ -10,65 +10,12 @@
 
 #include <dspapi.h>
 
-struct state {
-	STRM_Handle in_handle;
-	unsigned int in_size;
-	STRM_Handle out_handle;
-	unsigned int out_size;
-	uint8_t *in_buf;
-	uint8_t *out_buf;
-};
-
-void
-panic()
-{
-	int *beards;
-
-	beards = (void*) 0xFACEBEE5;
-	*beards = 0;
-	return;
-}
-
-int
 create(int arg_len, char *arg_str, int num_in_streams,
 		uint32_t in_stream_handles[], int num_out_streams,
 		uint32_t out_stream_handles[], NODE_EnvPtr node)
 {
-	STRM_Attrs attrs;
-	struct state *s;
-	RMS_StrmDef *strm_def;
 
-	s = MEM_valloc(0, sizeof(struct state), 0, 0);
-	node->moreEnv = s;
-	if (s == NULL)
-		return RMS_EOUTOFMEMORY;
-
-	strm_def = (RMS_StrmDef *)in_stream_handles[0];
-	attrs.nbufs = strm_def->nbufs;
-	attrs.segid = strm_def->segid;
-	attrs.timeout = strm_def->timeout;
-	attrs.align = strm_def->align;
-
-	s->in_handle = STRM_create(strm_def->name, STRM_INPUT,
-					strm_def->bufsize, &attrs);
-	s->in_size = strm_def->bufsize;
-
-	strm_def = (RMS_StrmDef *)out_stream_handles[0];
-	attrs.nbufs = strm_def->nbufs;
-	attrs.segid = strm_def->segid;
-	attrs.timeout = strm_def->timeout;
-	attrs.align = strm_def->align;
-
-	s->out_handle = STRM_create(strm_def->name, STRM_OUTPUT,
-					strm_def->bufsize, &attrs);
-	s->out_size = strm_def->bufsize;
-
-	s->in_buf = (void *)STRM_allocateBuffer(s->in_handle, s->in_size);
-	s->out_buf = (void *)STRM_allocateBuffer(s->out_handle,s->out_size);
-
-	if (s->in_buf == NULL || s->out_buf == NULL)
-		return RMS_EOUTOFMEMORY;
-
+	/* We have precisely no setup */
 	return 0;
 }
 
